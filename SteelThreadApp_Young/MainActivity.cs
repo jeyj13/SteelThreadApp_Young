df682e248db31672ac7e0de13684392a3e288788
Jacob Young <jeyj13@gmail.com>
2017-04-19 23:13:32 -0400

    4/19/2017 Finally solved issues with Xamarin and VS to produce a working copy of app. Currently returns a 400 error, but issue was expected

---
+++ SteelThreadApp_Young/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Text;
+using System.Net.Http;
+using System;
+
+namespace SteelThreadApp_Young
+{
+    [Activity(Label = "SteelThreadApp_Young", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Button btn1;
+        private EditText txt1;
+        private string editTextString;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+            txt1 = FindViewById<EditText>(Resource.Id.txtInput);
+            btn1 = FindViewById<Button>(Resource.Id.btnSend);
+            btn1.Click += btn1_Click;
+
+        }
+        protected override void OnStart()
+        {
+            base.OnStart();
+
+
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                editTextString = txt1.Text;
+                var url = string.Format("https://jacobproject.azurewebsites.net/api/HttpTriggerCSharp1?code=CF5a7FaDJ/SkohQ9H8q0gyZg4zsMIzya7VvxKliNW5nxITavR25TUA==");
+                var content = new StringContent("{passedString:'" + editTextString + "'}", Encoding.UTF8, "application/json");
+                client.PostAsync(url, content).Result.EnsureSuccessStatusCode();
+            }
+        }
+    }
+    }
+
